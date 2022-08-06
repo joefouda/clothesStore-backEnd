ff4c297cac4f5aae9d64f9c5b64a33c3570b82ec
gou181 <ya1999181@gmail.com>
2022-08-07 00:41:23 +0200

    finish main slider control

--- modules/other/otherController.js
+++ modules/other/otherController.js
@@ -7,6 +7,21 @@ const add = async(req,res,next)=>{
             { name:req.params.name },
             { $push: { photos: {id:uuid.v4(), src:req.body.photo} } },
             {new:true}
+        ); 
+        res.send({mainSlider})
+    }catch(error){
+        error.status = 422;
+        next(error)
+    }
+}
+
+
+const deletePhoto = async(req,res,next)=>{
+    try{
+        let mainSlider = await Other.findOneAndUpdate(
+            { name:req.params.name },
+            { $pull: { photos: {id:req.params.id} } },
+            {new:true}
         );
         res.send({mainSlider})
     }catch(error){
@@ -19,20 +34,6 @@ const getMainSliderPhotos = async(req,res,next)=>{
     try{
         let mainSlider = await Other.findOne(
             { name:req.params.name },
-        );
-        res.send({mainSlider})
-    }catch(error){
-        error.status = 422;
-        next(error)
-    }
-}
-
-const deletePhoto = async(req,res,next)=>{
-    try{
-        let mainSlider = await Other.findOneAndUpdate(
-            { name:req.params.name },
-            { $pull: { photos: {id:req.params.id} } },
-            {new:true}
         );
         res.send({mainSlider})
     }catch(error){
